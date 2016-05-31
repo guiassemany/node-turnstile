dd1fa2a56678cbfc011b19c2d23de4dc889e4778
Guilherme Assemany <guilhermeassemany@hotmail.com>
2016-05-31 16:49:51 -0300

    Added MySQL connection to test the turnstile

--- client.js
+++ client.js
@@ -1,17 +1,27 @@
 var net = require('net');
 
 var client = new net.Socket();
-client.connect(1337, '10.5.69.40', function() {
-//client.connect(2101, '10.5.71.234', function() {
+//client.connect(1337, '10.5.69.40', function() {
+client.connect(2101, '10.5.71.234', function() {
 	console.log('Conectado');
+});
+var resposta = "";
+client.on('data', function(data) {
 
-	var b = new Buffer('some string or other');
-	client.write(b);
+	resposta += data.toString();
+
+	if(resposta.length == 60 ){
+		console.log(resposta.toString().trim());
+		resposta = "";
+		client.destroy();
+	}
+
 });
 
-client.on('data', function(data) {
-	console.log('Recebido: ' + data);
-	//client.destroy();
+client.on('end', function() {
+	console.log('Conexao terminando');
+	//var b = new Buffer("XP");
+	//client.write(b);
 });
 
 client.on('close', function() {
--- server.js
+++ server.js
@@ -1,29 +1,44 @@
 var net = require('net');
 var StringDecoder = require('string_decoder').StringDecoder;
 var decoder = new StringDecoder('utf8');
+var mysql = require('mysql');
+
+var mysql      = require('mysql');
+var connection = mysql.createConnection({
+  host     : '10.5.70.129',
+  user     : 'root',
+  password : 'grk1957',
+  database : 'cap'
+});
 
 var server = net.createServer(function(socket) {
 
   console.log('CONECTADO: ' + socket.remoteAddress +':'+ socket.remotePort);
 
-  socket.write("Entrou!");
+  //socket.write("XP");
 	//socket.pipe(socket);
 
   socket.on('error', function(err) {
     console.log(err);
   });
 
+  var resposta = "";
   socket.on('data', function(data) {
-
-    a = data.toString('ascii');
-    b = decoder.write(data);
-    c = data.toString('utf8');
-    console.log("Dados: " + a);
-    console.log("Dados: " + b);
-    console.log("Dados: " + c);
-
-    socket.write("Envio de volta");
-
+    	resposta += data.toString();
+    	if(resposta.length == 60 ){
+    		console.log(resposta.toString().trim());
+        connection.connect();
+        connection.query("INSERT INTO teste (catraca) VALUES ('"+resposta.toString().trim()+"')", function(err, rows, fields) {
+          if (!err)
+            console.log('Inserido no BD');
+          else
+            console.log('Erro ao inserir.');
+        });
+        connection.end();
+    		resposta = "";
+    	}
+      //var b = new Buffer("XP");
+    	//socket.write(b);
   });
 
   socket.on('end', function() {
@@ -32,4 +47,4 @@ var server = net.createServer(function(socket) {
 
 });
 
-server.listen(1337, '10.5.69.40');
+server.listen(5000, '10.5.69.40');
