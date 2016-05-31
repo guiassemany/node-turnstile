4c2ecad9719e99a554c5f7cd00b7f712a61be01e
Guilherme Assemany <guilhermeassemany@hotmail.com>
2016-05-31 17:37:05 -0300

    Some small changes

--- client.js
+++ client.js
@@ -1,7 +1,6 @@
 var net = require('net');
 
 var client = new net.Socket();
-//client.connect(1337, '10.5.69.40', function() {
 client.connect(2101, '10.5.71.234', function() {
 	console.log('Conectado');
 });
