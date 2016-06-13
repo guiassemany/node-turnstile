5be484b8778ebc151d10c1924bc46761d1d430a2
Guilherme Assemany <guilhermeassemany@hotmail.com>
2016-06-13 13:16:04 -0300

    Improved

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 var net = require('net');
 var StringDecoder = require('string_decoder').StringDecoder;
 var decoder = new StringDecoder('utf8');
-var NetKeepAlive = require('net-keepalive');
+//var NetKeepAlive = require('net-keepalive');
 var Catraca = require('./catraca');
 var RealTime = require('./realtime/server');
 
@@ -33,10 +33,10 @@ var server = net.createServer(function(socket) {
     socket.setKeepAlive(habilitar, duracaoInicial); // seta SO_KEEPALIVE e TCP_KEEPIDLE
 
     var intervaloSonda = 1000; // após duracaoInicial envia sondas a cada 1 segundo
-    NetKeepAlive.setKeepAliveInterval(socket, intervaloSonda); // seta TCP_KEEPINTVL
+    //NetKeepAlive.setKeepAliveInterval(socket, intervaloSonda); // seta TCP_KEEPINTVL
 
     var maxSondasAntesDrop = 10; // Depois de 10 tentativas a conexão será dropada
-    NetKeepAlive.setKeepAliveProbes(socket, maxSondasAntesDrop); // seta TCP_KEEPCNT
+    //NetKeepAlive.setKeepAliveProbes(socket, maxSondasAntesDrop); // seta TCP_KEEPCNT
 
     /*
      * Tratamento de Erro do Socket
@@ -56,62 +56,64 @@ var server = net.createServer(function(socket) {
         resposta += data.toString();
         if (resposta.length == 60) {
             console.log(resposta);
-            Catraca.montaResposta60(resposta.replace(/\0/g, ''));
-            console.log(Catraca.infoAcesso);
-            Catraca.verificaCartao(Catraca.infoAcesso.abaTrack, function(statusCartao) {
-                if (statusCartao.bloqueado === true) {
-                    socket.write("!NN Bloqueado      A000000.......*");
-                } else if (statusCartao.supervisor === true && statusCartao.bloqueado === false) {
-                    if (Catraca.infoAcesso.leitor == "1") { // Se Supervisor colocar no leitor 1
-                        socket.write("!OK Supervisor     A000000.......*");
-                    } else { // Se Supervisor colocar no leitor 2 = Bloqueia
-                        socket.write("!NN Bloqueado      A000000.......*");
-                    }
-                } else {
-                    if (statusCartao.funcionario === true) { // Funcionário
-                        if (Catraca.infoAcesso.leitor == "1") { // Se Funcionário colocar no leitor 1
-                            Catraca.verificaUltimoAcesso(Catraca.infoAcesso.abaTrack, function(sentidoUltAcesso) {
-                                if (sentidoUltAcesso == 'E') { // Último acesso Entrada = Libera Saída
-                                    socket.write("!OK Ate logo!      S000000.......*");
-                                } else if (sentidoUltAcesso == 'S') { // Último acesso Saída = Libera entrada
-                                    socket.write("!OK Bem vindo      E000000.......*");
-                                } else { // Sem último acesso = Libera Ambos
-                                    socket.write("!OK Bem vindo      A000000.......*");
-                                }
-                            });
-                        } else { // Se Funcionário colocar no leitor 2 = Bloqueia
-                            socket.write("!NN Bloqueado      A000000.......*");
-                        }
-                    } else { // Visitante
-                        if (Catraca.infoAcesso.leitor == "1") {
-                            Catraca.verificaUltimoAcesso(Catraca.infoAcesso.abaTrack, function(sentidoUltAcesso) {
-                                if (sentidoUltAcesso == 'E') {
-                                    socket.write("!NN Bloqueado      A000000.......*");
-                                } else {
-                                    socket.write("!OK Bem vindo      E000000.......*");
-                                }
-                            });
-                        } else {
-                            socket.write("!OK Ate logo!      S000000.......*");
-                        }
-                    }
-                    Catraca.limpaInfoAcesso();
-                }
+            Catraca.montaResposta60(resposta.replace(/\0/g, ''), function(infoAcesso){
+              console.log(Catraca.infoAcesso);
+              Catraca.verificaCartao(Catraca.infoAcesso.abaTrack, function(statusCartao) {
+                  if (statusCartao.bloqueado === true) {
+                      socket.write("!NN Bloqueado      A000000.......*");
+                  } else if (statusCartao.supervisor === true && statusCartao.bloqueado === false) {
+                      if (Catraca.infoAcesso.leitor == "1") { // Se Supervisor colocar no leitor 1
+                          socket.write("!OK Supervisor     A000000.......*");
+                      } else { // Se Supervisor colocar no leitor 2 = Bloqueia
+                          socket.write("!NN Bloqueado      A000000.......*");
+                      }
+                  } else {
+                      if (statusCartao.funcionario === true) { // Funcionário
+                          if (Catraca.infoAcesso.leitor != "2") { // Se Funcionário colocar no leitor 1
+                              Catraca.verificaUltimoAcesso(Catraca.infoAcesso.abaTrack, function(sentidoUltAcesso) {
+                                  if (sentidoUltAcesso == 'E') { // Último acesso Entrada = Libera Saída
+                                      socket.write("!OK Ate logo!      S000000.......*");
+                                  } else if (sentidoUltAcesso == 'S') { // Último acesso Saída = Libera entrada
+                                      socket.write("!OK Bem vindo      E000000.......*");
+                                  } else { // Sem último acesso = Libera Ambos
+                                      socket.write("!OK Bem vindo      A000000.......*");
+                                  }
+                              });
+                          } else { // Se Funcionário colocar no leitor 2 = Bloqueia
+                              socket.write("!NN Bloqueado      A000000.......*");
+                          }
+                      } else { // Visitante
+                          if (Catraca.infoAcesso.leitor == "1") {
+                              Catraca.verificaUltimoAcesso(Catraca.infoAcesso.abaTrack, function(sentidoUltAcesso) {
+                                  if (sentidoUltAcesso == 'E') {
+                                      socket.write("!NN Bloqueado      A000000.......*");
+                                  } else {
+                                      socket.write("!OK Bem vindo      E000000.......*");
+                                  }
+                              });
+                          } else {
+                              socket.write("!OK Ate logo!      S000000.......*");
+                          }
+                      }
+                      Catraca.limpaInfoAcesso();
+                  }
+              });
+              resposta = "";
             });
-            resposta = "";
         } else if (resposta.length == 58) {
             console.log(resposta);
-            Catraca.montaResposta58(resposta.replace(/\0/g, ''));
-            Catraca.gravaAcessoCatraca(Catraca.infoAcesso, function(resultado, nome, foto) {
-                Catraca.infoAcesso.nome = nome;
-                Catraca.infoAcesso.foto = foto;
-                console.log(Catraca.infoAcesso);
-                RealTime.sockets.emit('nova-movimentacao', {
-                    infoAcesso: Catraca.infoAcesso
-                });
-                Catraca.limpaInfoAcesso();
+            Catraca.montaResposta58(resposta.replace(/\0/g, ''), function(infoAcesso){
+              Catraca.gravaAcessoCatraca(Catraca.infoAcesso, function(resultado, nome, foto) {
+                  Catraca.infoAcesso.nome = nome;
+                  Catraca.infoAcesso.foto = foto;
+                  console.log(Catraca.infoAcesso);
+                  RealTime.sockets.emit('nova-movimentacao', {
+                      infoAcesso: Catraca.infoAcesso
+                  });
+                  Catraca.limpaInfoAcesso();
+              });
+              resposta = "";
             });
-            resposta = "";
         }
     });
 
