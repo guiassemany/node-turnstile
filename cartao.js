10d1b7a0835fdc18e893772fe2c66f4169457c53
Guilherme Assemany <guilhermeassemany@hotmail.com>
2016-06-17 10:08:14 -0300

    Change .env key

--- cartao.js
+++ cartao.js
@@ -3,7 +3,7 @@ var moment = require('moment');
 var mysql = require('mysql');
 
 var connection = mysql.createConnection({
-  host: process.env.DB_HOST_PROD,
+  host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_DATABASE
